38e9fbf6f4950cab7d2f09d2e4ba6c5aa4e0ba71
SilvanoSCF <sscf.prog@gmail.com>
2022-12-29 15:17:20 -0300

    Alterando Classe de variaveis

--- ProjetoJavaDio/src/MinhaClasse.java
+++ ProjetoJavaDio/src/MinhaClasse.java
@@ -1,5 +1,18 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("Olá turma, sejam bem vindos ...");
+
+        String primeiroNome = "Silvano";
+        String segundoNome = "Filho";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
     }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+
 }
