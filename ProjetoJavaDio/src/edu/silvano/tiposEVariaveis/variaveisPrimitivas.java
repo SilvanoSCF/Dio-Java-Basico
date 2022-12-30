d94d54108117bc3628df285a753ee387f8540876
SilvanoSCF <sscf.prog@gmail.com>
2022-12-30 10:18:16 -0300

    Tipos e Variaveis

---
+++ ProjetoJavaDio/src/edu/silvano/tiposEVariaveis/variaveisPrimitivas.java
@@ -0,0 +1,11 @@
+package edu.silvano.tiposEVariaveis;
+
+public class variaveisPrimitivas {
+
+
+    byte numero1 = 1; // 1 byte
+    short numero2 = 2; // 2 bytes    
+    int numero4 = 4; // 4 bytes
+    long numero8 = 8; // 8 bytes
+    
+}
