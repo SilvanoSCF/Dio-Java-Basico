430145a63b1e1312b7bc102f835bb2a256e2c1de
SilvanoSCF <sscf.prog@gmail.com>
2022-12-30 13:48:11 -0300

    Variaveis Primitivas

--- ProjetoJavaDio/src/edu/silvano/tiposEVariaveis/variaveisPrimitivas.java
+++ ProjetoJavaDio/src/edu/silvano/tiposEVariaveis/variaveisPrimitivas.java
@@ -2,10 +2,40 @@ package edu.silvano.tiposEVariaveis;
 
 public class variaveisPrimitivas {
 
+    // Variáveis primitivas numericas
 
     byte numero1 = 1; // 1 byte
-    short numero2 = 2; // 2 bytes    
+    short numero2 = 2; // 2 bytes
     int numero4 = 4; // 4 bytes
-    long numero8 = 8; // 8 bytes
-    
+    long numero8 = 8L; // 8 bytes Lembrando que o tipo long finaliza em 'L'
+
+    // Variáveis primitivas numeral
+
+    float numeral = 4.0F; // 4 bytes Lembrando que o tipo float finaliza em 'F'
+    double numeal2 = 8.0; // 8 bytes
+
+    String texto01 = "texto"; 
+
+
+     //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+     // Trabalhando com Constantes
+     // São valores armazenados em memória que não podem ser modificados depois de declarados.
+     // Em Java, esses valores são representados pela palavra reservada 'final', seguida do tipo.
+     // por conversão, Constantes são sempre escritas em caixa alta. Nomenclatura UPPER_CASE
+
+     final double VALOR_DE_PI = 3.14; // Pi é um valor unico não pode ser alterado.
+
+
+    public static void main(String[] args) {
+
+        //double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // Convertendo para um tipo mais especifico - Casting
+
+       
+              
+    }
+
 }
