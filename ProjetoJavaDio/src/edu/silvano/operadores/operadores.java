457b581458a67cd3bc6426397bfad81fa0ea719c
SilvanoSCF <sscf.prog@gmail.com>
2022-12-31 17:19:47 -0300

    operadores aritméticos

---
+++ ProjetoJavaDio/src/edu/silvano/operadores/operadores.java
@@ -0,0 +1,16 @@
+package edu.silvano.operadores;
+
+public class operadores {
+
+    String nome = "Silvano"; // = Atribuição
+
+    // Aritméticos
+
+    double soma = 10.5 + 15.7;
+    int subtracao = 113 - 15;
+    int multiplicacao = 20 * 3;
+    int divisao = 15 / 3;
+    int modulo = 18 % 3;
+    double resultado = (10 * 7) + (20 / 4);
+
+}
