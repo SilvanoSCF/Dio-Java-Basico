c75ac19a836f05eab1c1454a023ea2eaa6c069fb
SilvanoSCF <sscf.prog@gmail.com>
2022-12-31 17:26:25 -0300

    Operadores

--- ProjetoJavaDio/src/edu/silvano/operadores/operadores.java
+++ ProjetoJavaDio/src/edu/silvano/operadores/operadores.java
@@ -2,15 +2,24 @@ package edu.silvano.operadores;
 
 public class operadores {
 
-    String nome = "Silvano"; // = Atribuição
+    public static void main(String[] args) {
 
-    // Aritméticos
+        String nome = "Silvano"; // = Atribuição
 
-    double soma = 10.5 + 15.7;
-    int subtracao = 113 - 15;
-    int multiplicacao = 20 * 3;
-    int divisao = 15 / 3;
-    int modulo = 18 % 3;
-    double resultado = (10 * 7) + (20 / 4);
+        // Aritméticos
+
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 15;
+        int multiplicacao = 20 * 3;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20 / 4);
+
+        // Concatenação
+
+        String valores = "Soma: " + soma + ", Subtração " + subtracao;
+
+        System.out.println(valores);
+    }
 
 }
