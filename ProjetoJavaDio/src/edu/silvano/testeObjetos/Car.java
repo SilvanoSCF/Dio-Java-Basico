01ebc69d92ffe000a2d4c9517bece9761e1c1980
SilvanoSCF <sscf.prog@gmail.com>
2023-01-19 00:34:07 -0300

    Objeto Car

---
+++ ProjetoJavaDio/src/edu/silvano/testeObjetos/Car.java
@@ -0,0 +1,66 @@
+package edu.silvano.testeObjetos;
+
+/**
+ * <h1>Car</h1>
+ * 
+ * @author Silvano filho
+ */
+public class Car {
+
+    private String modelo;
+    private String cor;
+    private int capacidadeTanque;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    Double calculoTotalDespesa(int capacidadeTanque) {
+        return capacidadeTanque * 4.5;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+    public Car() {
+
+    }
+
+    /**
+     * <h2>Método Construtor Car</h2>
+     * 
+     * @param modelo
+     * @param cor
+     * @param capacidadeTanque
+     */
+    public Car(String modelo, String cor, int capacidadeTanque) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.capacidadeTanque = capacidadeTanque;
+        System.out.println("Iniciando método construtor de Car");
+    }
+
+}
---
+++ ProjetoJavaDio/src/edu/silvano/x003POODefinicao/carro002.java
@@ -0,0 +1,49 @@
+package edu.silvano.x003POODefinicao;
+
+/**
+ * <h1>Carro002</h1>
+ * Classe objeto carro
+ * @author Silvano filho
+ *
+ */
+public class carro002 {
+
+    private String cor;
+    private String modelo;
+    private int capacidadeTanque; 
+
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+
+
+
+
+    
+
+    //Public Carro002();
+
+
+}
+
+
+
+
