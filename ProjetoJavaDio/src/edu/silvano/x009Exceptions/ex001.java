328343cdf7089be2e84e821d2c16d7ee5906eb19
SilvanoSCF <sscf.prog@gmail.com>
2023-02-21 19:06:02 -0300

    Exceptions 002

--- ProjetoJavaDio/src/edu/silvano/x009Exceptions/ex001.java
+++ ProjetoJavaDio/src/edu/silvano/x009Exceptions/ex001.java
@@ -1,5 +1,11 @@
 public class ex001 {
 
-    // ex001
+    // error problema no recurso do programa não permitindo a execução
+    // ex caso apareça um erro uncheched algum atributo, divisão, operação não permitida
+    // checked exception, excessoes que devem ser evitados e tratados pelo programador para que o programa funcione
+
+    
+
+
 
 }
