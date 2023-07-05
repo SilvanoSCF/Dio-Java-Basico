2a8d8bb6169799cb7e8ef40ba17ce07bcdc34e8a
SilvanoSCF <sscf.prog@gmail.com>
2023-07-04 23:04:34 -0300

    Aula 3 JavaScript

--- CFBCursos_Javascript/PrimeirosPassos/Aula002.js
+++ CFBCursos_Javascript/PrimeirosPassos/Aula002.js
@@ -1,5 +1,10 @@
+"use strict" // Modo Estrito restringe possiveis erros
+
 // Declaração de variaveis
-let nome = "Silvano"
+ let nome = "Silvano"
+//Impressão 
 console.log(nome)
+//Concatenação
+console.log("Nome: "+nome)
 
 
