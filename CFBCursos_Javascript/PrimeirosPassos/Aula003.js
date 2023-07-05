d89ffe649fd8a6c801ca77eefc1e40d5481e0846
SilvanoSCF <sscf.prog@gmail.com>
2023-07-04 23:24:39 -0300

    Aula 003 fim

--- CFBCursos_Javascript/PrimeirosPassos/Aula003.js
+++ CFBCursos_Javascript/PrimeirosPassos/Aula003.js
@@ -1,3 +1,15 @@
  "use strict"
 
- var nome = "Silvano"
+ let nome = "Silvano"
+
+nome = "Elisangela"
+
+nome = 10;
+
+console.log(nome)
+
+const curso = "javascript"
+
+curso = "html"
+
+console.log(curso)
