2eeccd09cd54d69ac3a4396ffb26b37108542502
SilvanoSCF <sscf.prog@gmail.com>
2023-07-06 10:08:23 -0300

    Aula 004 Variaveis

---
+++ CFBCursos_Javascript/PrimeirosPassos/Aula004.js
@@ -0,0 +1,27 @@
+// Operadores Aritiméticos
+
+/**
+ * + Adição +
+ * - Subtração -
+ * / Divisão /
+ * * Multiplicação *
+ * % Resto de divição
+ *  ++ ou += Incremento
+ *  -- ou -= Decremento
+ * 
+ * 
+ */
+
+
+// Declaração de variaveis
+
+let num1, num2, res;
+
+num1 = 5
+num2 = 4
+res = num1+num2
+
+console.log("Primeiro valor: " + num1)
+console.log("Segundo Valor:  " + num2)
+console.log("Resultado da soma dos valores: " + res)
+
