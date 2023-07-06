447177c75982fd4b1e44ed6289731249ff6077e5
SilvanoSCF <sscf.prog@gmail.com>
2023-07-06 10:32:17 -0300

    Aula 005 Operadores Relacionais

---
+++ CFBCursos_Javascript/PrimeirosPassos/Aula005.js
@@ -0,0 +1,27 @@
+// Operadores Relacionais
+
+// Operadores de comparação
+
+/**
+ * > Maior
+ * >= Maior ou igual
+ * < Menor
+ * <= Menor ou igual
+ * = Igual atribuição
+ * == Igual igual comparação
+ * != Exclamação igual  Operador de Negação
+ * 
+ * 
+ */
+
+let num1 = 10;
+let num2 = 15;
+let num3 = 20;
+let num4 = 25;
+
+console.log(num1 < num4)
+
+// Testando a inverção
+
+console.log(!(num1 == num2)) // neste caso o operador de negação inverteu o resultado falso que num1 é diferente de num2
+
