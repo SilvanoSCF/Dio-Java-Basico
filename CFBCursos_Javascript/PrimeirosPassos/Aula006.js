96d75b1b58c7e387ac15280825ed122e7f7e4cb4
SilvanoSCF <sscf.prog@gmail.com>
2023-07-08 18:26:40 -0300

    Aula007

--- CFBCursos_Javascript/PrimeirosPassos/Aula006.js
+++ CFBCursos_Javascript/PrimeirosPassos/Aula006.js
@@ -1,6 +1,20 @@
 // Duvidas
 
-// Diferença entre == e ===
+// Diferença entre == e === exemplo
 
+let num1 = 1
+let num2 = "1"
 
+let n1 = 1
+let n2 = 2
+
+console.log(num1 == num2) // true por que o == faz uma comparação pelo valor da variavel e não pelo tipo
+
+console.log(num1 === num2) // False porque o nivel de exigencia é maior, abrangendo ate tipo
+
+let nome = prompt("Escreva o seu nome: ")
+
+console.log(nome)
+
+console.log(!(n1 != n2))   // != diferente     ! negação
 
---
+++ CFBCursos_Javascript/PrimeirosPassos/Aula007.js
@@ -0,0 +1,20 @@
+/**
+ *  Operadores Lógicos
+ * 
+ *  || or
+ *  && And
+ *  ! Not
+ * 
+ */
+
+let n1, n2, n3, n4
+
+n1 = 102
+n2 = 15
+n3 = 20
+n4 = 25
+
+if ((n1 < n2) || (n2 > n3))
+    console.log("Verdadeiro")
+else
+    console.log("Falso")
