821eca60c4c0628527534f739116f183a3d9b482
SilvanoSCF <sscf.prog@gmail.com>
2023-07-16 00:00:52 -0300

    Aula011

---
+++ CFBCursos_Javascript/PrimeirosPassos/Aula010.js
@@ -0,0 +1,21 @@
+// Operador ternário
+
+let n = 10
+
+res=(!(n%2) ? "Numero Par" : "Numero Impar")
+
+console.log(res)
+
+// 0 = False
+
+// 1 = True
+
+// Teste Lógico ? se verdadeiro : se falso
+
+//////////////////////////////////////////////
+
+let n2 = 11
+
+res = (n > n2 ? "Verdadeiro" : "Falso")
+
+console.log(res)
