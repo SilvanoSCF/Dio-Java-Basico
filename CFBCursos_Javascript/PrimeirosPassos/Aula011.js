c7b9bc88f8502784a59701b3972d7ec8e61a4261
SilvanoSCF <sscf.prog@gmail.com>
2023-07-25 00:57:59 -0300

    Spread Aula 12

--- CFBCursos_Javascript/PrimeirosPassos/Aula011.js
+++ CFBCursos_Javascript/PrimeirosPassos/Aula011.js
@@ -1,2 +1,12 @@
 // Operador Tipeof
 
+let v1 = "Silvano"
+let v2 = 1
+let v3 = 1.12
+let v4 = false
+let v5 = 
+
+console.log(typeof(v1))
+console.log(typeof(v2))
+console.log(typeof(v3))
+console.log(typeof(v4))
---
+++ CFBCursos_Javascript/PrimeirosPassos/Aula012.js
@@ -0,0 +1,14 @@
+// Exemplo operadoR Spread
+
+let n1 = [10, 11, 12, 13] 
+let n2 = ["Silvano", "Elisangela", "Ellen", "Elis Vitória"]
+let n3 = 0
+
+console.log(n1)
+console.log(n2)
+console.log(n3)
+
+let valores=[n1,n2,n3]
+
+console.log(...valores)
+console.log(n1+n2+n3)
